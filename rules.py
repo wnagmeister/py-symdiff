0aecc8ae86cc44ee59deff75b2fd64c797413931
yinghao <231103865+wnagmeister@users.noreply.github.com>
2025-10-02 18:18:10 +0100

    WIP rules refactor

--- rules.py
+++ rules.py
@@ -3,7 +3,8 @@ from astree import AstNode
 
 
 class PatternVariable(Variable):
-    def __init__(self, string, match_type=None):
+    def __init__(self, string, match_type: None | float = None):
+        """If a match type is not given, it is inferred from the string."""
         super().__init__(string)
         if not match_type:
             self.match_type = self.default_match_type(string)
@@ -13,14 +14,15 @@ class PatternVariable(Variable):
         match string:
             case "s":
                 return float
-            case "o":
-                return Operator
-            case "v":
-                return Variable
-            case "l":
-                return "literal"
             case _:
                 return "all"
+
+    def match(self, expr: AstNode) -> bool:
+        """Returns whether the PatternVariable matches expr at the root."""
+        if self.match_type == "all":
+            return True
+        else:
+            return isinstance(expr.value, self.match_type)
 
 
 class Rule:
@@ -31,14 +33,6 @@ class Rule:
         self.pattern = pattern
         self.replacement = replacement
 
-    # This substitution method does not work for arbitrary variables
-    # eg is we have bound f, g, once we replace f, the replacement
-    # might contain g's, which we do not want to replace.
-    # Better might be a way to find all the nodes(and parents) which
-    # need to be replaced, then replace all at once, i.e. no interweaving
-    # replacements and searches.
-    # or does it work??? once a node is subbed in, its skipped
-
     def apply(self, expr: AstNode) -> bool:
         """Applies the rule in place onto the expression expr if possible. The
         rule must match to the whole of expr; it does not match it to
@@ -48,7 +42,7 @@ class Rule:
         bindings: dict[Variable, AstNode] = {}
         if is_match(expr, self.pattern, bindings):
             replacement: AstNode = self.replacement.copy()
-            substitute(replacement, bindings)
+            replacement.substitute_variables(bindings)
             expr.value = replacement.value
             expr.children = replacement.children
             return True
@@ -112,20 +106,36 @@ def is_match(
                 return True
 
 
-def substitute(expr: AstNode, bindings: dict[Variable, AstNode]) -> None:
-    """Substitutes variables in expr in place according to the bindings
-    given.
-    """
-    if expr.is_leaf() and (binding := bindings.get(expr.value)):
-        expr.value = binding.value
-        expr.children = binding.children
-    else:
-        for i, child in enumerate(expr.children):
-            if child.is_leaf():
-                if binding := bindings.get(child.value):
-                    expr.children[i] = binding
+def match2(
+    expr: AstNode, pattern: AstNode, bindings: dict[PatternVariable, AstNode]
+) -> bool:
+    match pattern.value:
+        case float():
+            return expr.value == pattern.value
+
+        case PatternVariable():
+            if pattern.value.match_type == "all" or isinstance(
+                expr.value, pattern.value.match_type
+            ):
+                if existing_binding := bindings.get(pattern.value):
+                    return expr.is_equal(existing_binding)
+                else:
+                    bindings[pattern.value] = expr
+                    return True
             else:
-                substitute(child, bindings)
+                return False
+
+        case Variable():
+            return expr.value == pattern.value
+
+        case _:
+            if not isinstance(expr.value, Operator) or expr.value != pattern.value:
+                return False
+            else:
+                for expr_child, pattern_child in zip(expr.children, pattern.children):
+                    if not is_match(expr_child, pattern_child, bindings):
+                        return False
+                return True
 
 
 rules: set[Rule] = {
